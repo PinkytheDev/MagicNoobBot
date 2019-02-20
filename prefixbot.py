0a0dd316970583ea66d7b1253f2f79a1eceade18
PinkytheDev <47577706+PinkytheDev@users.noreply.github.com>
2019-02-21 00:01:42 +0200

    Update prefixbot.py

--- prefixbot.py
+++ prefixbot.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 
+
+import datetime
+
 client = commands.Bot(command_prefix=".")
 
 TOKEN = "NDY0ODMxMzI4MjYxNjM2MDk2.DiE2YQ.j3jTWdwAJ8WVZPCSXVUKBG3-vu0"
@@ -10,6 +13,42 @@ async def on_ready():
 	print ("Logged in as")
 	print (client.user.name)
 	print ("---------------")
+	
+	
+@client.event
+async def on_message(message):
+	user = message.author
+	msg = message.content
+	print (f"{user} : {msg}")
+	await client.process_commands(message)
+	
+	
+	
+@client.event
+async def on_message_delete(message):
+    author = message.author
+    content = message.content
+    channel = message.channel
+    await client.send_message(channel, '{}: {}'.format(author, content))
+    await client.process_commands(message)
+	
+	
+	
+@client.command()
+async def ping ():
+    await client.say('Pong!')
+    await client.process_commands(message)
+
+	
+			
+@client.command()
+async def echo(*args):
+    output = ''
+    for word in args:
+        output += word
+        output += ' '
+    await client.say(output)
+    await client.process_commands(message)
 
 		
 client.run(TOKEN)
