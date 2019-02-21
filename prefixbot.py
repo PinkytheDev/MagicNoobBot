cc98dadb1b4e62e81f0f1f9c6c235ad8036a7d94
PinkytheDev <47577706+PinkytheDev@users.noreply.github.com>
2019-02-21 17:02:49 +0200

    Update prefixbot.py

--- prefixbot.py
+++ prefixbot.py
@@ -1,29 +1,28 @@
 import discord
 from discord.ext import commands
 
-
 import datetime
 
 client = commands.Bot(command_prefix=".")
 
 TOKEN = "NDY0ODMxMzI4MjYxNjM2MDk2.DiE2YQ.j3jTWdwAJ8WVZPCSXVUKBG3-vu0"
 
+
 @client.event
 async def on_ready():
-	print ("Logged in as")
-	print (client.user.name)
-	print ("---------------")
-	
-	
+    print("Logged in as")
+    print(client.user.name)
+    print("---------------")
+
+
 @client.event
 async def on_message(message):
-	user = message.author
-	msg = message.content
-	print (f"{user} : {msg}")
-	await client.process_commands(message)
-	
-	
-	
+    user = message.author
+    msg = message.content
+    print(f"{user} : {msg}")
+    await client.process_commands(message)
+
+
 @client.event
 async def on_message_delete(message):
     author = message.author
@@ -31,16 +30,14 @@ async def on_message_delete(message):
     channel = message.channel
     await client.send_message(channel, '{}: {}'.format(author, content))
     await client.process_commands(message)
-	
-	
-	
+
+
 @client.command()
-async def ping ():
+async def ping():
     await client.say('Pong!')
     await client.process_commands(message)
 
-	
-			
+
 @client.command()
 async def echo(*args):
     output = ''
@@ -49,13 +46,18 @@ async def echo(*args):
         output += ' '
     await client.say(output)
     await client.process_commands(message)
-    
-    
-@client.command()
-asyn def embed(ctx):
-	embed = discord.Embed(title="Title", description="Description", colour=discord.Color.green(), url="https://www.google.com")
-	
-	await ctx
 
-		
+
+@client.command()
+
+
+asyn
+
+
+def embed(ctx):
+    embed = discord.Embed(title="Title", description="Description", colour=discord.Color.green(), url="https://www.google.com")
+
+    await ctx.send(embed=embed)
+
+
 client.run(TOKEN)
