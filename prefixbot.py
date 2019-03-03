98077e396ab0aa1cd4fe6f66a0309421816d1b21
PinkytheDev <47577706+PinkytheDev@users.noreply.github.com>
2019-03-03 20:52:52 +0200

    Update prefixbot.py

--- prefixbot.py
+++ prefixbot.py
@@ -18,7 +18,6 @@ async def on_message(message):
     author = message.author
     content = message.content
     print('{}: {}'.format(author, content))
-    await client.process_commands(message)
 
 @client.event
 async def on_message_delete(message):
@@ -43,5 +42,14 @@ async def say(*args):
         output += word
         output += ' '
     await client.say(output)
+
+@client.command(pass_context=True, adminstator=True)
+async def clear(ctx, amount=100):
+    channel = ctx.message.channel
+    messages = []
+    async for message in client.logs_from(channel, limit=int(amount) + 1):
+        messages.append(message)
+    await client.delete_messages(messages)
+    await client.say("{} Messages Deleted".format(int(amount)) 
 
 client.run(TOKEN)
