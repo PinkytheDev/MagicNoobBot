b82dc84982759b04a45c947ea5a347659b31a9c6
PinkytheDev <47577706+PinkytheDev@users.noreply.github.com>
2019-03-03 19:59:50 +0200

    Update prefixbot.py

--- prefixbot.py
+++ prefixbot.py
@@ -8,9 +8,23 @@ client = commands.Bot(command_prefix = '.m')
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name='MagicNoob | .mhelp'))
+    await client.change_presence(game=Game(name='MagicNoob | .m'))
     print('The bot is connected')
     print('Discord Application is being online')
     print('Connected on Application ' + client.user.name)
+
+@client.event
+async def on_message(message):
+    author = message.author
+    content = message.content
+    print('{}: {}'.format(author, content))
+    
+@client.event
+async def on_message_delete(message):
+    author = message.author
+    content = message.content
+    channel = message.channel
+    await client.send_message(channel, '@{} has deleted a message'.format(author))
+    print('Deleted message > {}: {}'.format(author, content))
 
 client.run(TOKEN)
