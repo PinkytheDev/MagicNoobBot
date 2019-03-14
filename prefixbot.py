43b03abc4bbe207004fd572718a56fc1831ca252
PinkytheDev <47577706+PinkytheDev@users.noreply.github.com>
2019-03-14 19:16:52 +0200

    Update prefixbot.py

--- prefixbot.py
+++ prefixbot.py
@@ -1,8 +1,12 @@
 import discord
+import requests
+import random
+import asyncio
 from discord import Game
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.m')
+client = commands.Bot(command_prefix='.m')
+
 
 @client.event
 async def on_ready():
@@ -11,19 +15,23 @@ async def on_ready():
     print('Discord Application is being online')
     print('Connected on Application ' + client.user.name)
 
+
 @client.event
 async def on_message(message):
     author = message.author
     content = message.content
     print('{}: {}'.format(author, content))
     await client.process_commands(message)
-    
+
+
 @client.event
 async def on_member_join(member):
-	roles = discord.utils.get(member.server.roles, name='Members')
-	await client.add_roles(member, role)
-	await client.say("Hey! Server Owner. But if you don't have **Members** role. Then, I prefer you to add it to make this function work.")
-	await client.process_commands(message)
+    roles = discord.utils.get(member.server.roles, name='Members')
+    await client.add_roles(member, role)
+    await client.say(
+        "Hey! Server Owner. But if you don't have **Members** role. Then, I prefer you to add it to make this function work.")
+    await client.process_commands(message)
+
 
 @client.event
 async def on_message_delete(message):
@@ -33,12 +41,14 @@ async def on_message_delete(message):
     print('Deleted Message > {}: {}'.format(author, content))
     await client.process_commands(message)
 
+
 @client.command()
 async def infobot():
     await client.say('Name: MagicNoob')
     await client.say('Role: Supreme')
     await client.say('Owner: Join My Minecraft Server or Mr Noob Pink')
     await client.say('Main Server: MagicNoob')
+
 
 @client.command()
 @commands.has_permissions(administrator=True)
@@ -49,6 +59,7 @@ async def say(*args):
         output += ' '
     await client.say(output)
 
+
 @client.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 async def clear(ctx, amount=100):
@@ -58,5 +69,29 @@ async def clear(ctx, amount=100):
         messages.append(message)
     await client.delete_messages(messages)
     await client.say('Deleted Message(s)')
+
+@client.command (name='8ball',
+                                    description="Answers a yes/no question.",
+                                    brief="Answers from the beyond. Do .mhelp 8ball for more informations.",
+                                    aliases= ['eight_ball', 'eightball', '8_ball','8','Mr Noob Pink'],
+                                    pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+    'That is a resounding no',
+    'It is not looking likely',
+    'Too hard to tell',
+    'It is quite possible',
+    'Definitely',
+    'No',
+    'This question is shit',
+    'Fuck U',
+    'Yes',
+    'Hmmm???',
+    'I dont think so!',
+    'You Know What BYE!',
+    'I dont know',
+    ]
+
+await client.say(random.choice(possible_responses) + "," + context.message.author.mention)
 
 client.run('NDY0ODMxMzI4MjYxNjM2MDk2.D12f6w.JqdtRBcotRl8axCIQnNrQyaO7Y8')
