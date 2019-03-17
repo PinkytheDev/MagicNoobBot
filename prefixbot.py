22519a0435ef3f28e8dc8575602a5a7f21508c6d
PinkytheDev <47577706+PinkytheDev@users.noreply.github.com>
2019-03-17 23:30:37 +0200

    Update prefixbot.py

--- prefixbot.py
+++ prefixbot.py
@@ -47,47 +47,7 @@ async def on_member_join(member):
         "Hey! Server Owner. But if you don't have **Members** role. Then, I prefer you to add it to make this function work.")
     await client.process_commands(message)
     
-@client.event
-async def on_member_join(member):
-	with open('users.json', 'r') as f:
-		users = json.load(f)
-		
-	await update_data(users, member)
-	
-	with open('users.json', 'w') as f:
-		json.dump(users, f)
-	await client.process_commands(message)
-	
-@client.event
-async def on_message(message):
-	with open('users.json', 'r') as f:
-		users = json.load(f)
-		
-	await update_data(users, message.author)
-	await add_experience(users, message.author, 5)
-	await level_up(users, message.author, message.channel)
-	
-	with open('users.json', 'w') as f:
-		json.dump(users, f)
-		
-async def update_data(users, user):
-	if not user.id in users:
-		users[user.id] = {}
-		users[user.id]['experience'] = 0
-		users[user.id]['level'] = 0
-		
-async def add_experience(users, user, exp):
-	users[user.id]['experience'] += exp
-	
-async def level_up(users, user, channel):
-	experience = users[user.id]['experience']
-	lvl_start = users[user.id]['level']
-	lvl_end = int(experience ** (1/4))
-	
-	if lvl_start < lvl_end:
-		await client.say(channel, "{} has leveled up to {}".format(user.mention, lvl_end))
-		users[user.id]['level'] = lvl_end
-		await client.process_commands(message)
+
 	
 		
 @client.event
