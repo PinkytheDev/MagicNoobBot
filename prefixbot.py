4d2e55d4472ecffc5fb97dfb7f472278eee27949
PinkytheDev <47577706+PinkytheDev@users.noreply.github.com>
2019-03-17 22:34:36 +0200

    Update prefixbot.py

--- prefixbot.py
+++ prefixbot.py
@@ -12,6 +12,7 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix='.m')
 client.remove_command('help')
+os.chdir(r'https://github.com/PinkytheDev/MagicNoobBot')
 
 players = {}
 queues = {}
@@ -45,8 +46,50 @@ async def on_member_join(member):
     await client.say(
         "Hey! Server Owner. But if you don't have **Members** role. Then, I prefer you to add it to make this function work.")
     await client.process_commands(message)
-
-
+    
+@client.event
+async def on_member_join(member):
+	with open('users.json', 'r') as f:
+		users = json.load(f)
+		
+	await update_data(users, member)
+	
+	with open('users.json', 'w') as f:
+		json.dump(users, f)
+	await client.process_commands(message)
+	
+@client.event
+async def on_message(message):
+	with open('users.json', 'r') as f:
+		users = json.load(f)
+		
+	await update_data(users, message.author)
+	await add_experience(users, message.author, 5)
+	await level_up(users, message.author, message.channel)
+	
+	with open('users.json', 'w') as f:
+		json.dump(users, f)
+		
+async def update_data(users, user):
+	if not user.id in users:
+		users[user.id] = {}
+		users[user.id]['experience'] = 0
+		users[user.id]['level'] = 0
+		
+async def add_experience(users, user, exp):
+	users[user.id]['experience'] += exp
+	
+async def level_up(users, user, channel):
+	experience = users[user.id]['experience']
+	lvl_start = users[user.id]['level']
+	lvl_end = int(experience ** (1/4))
+	
+	if lvl_start < lvl_end:
+		await client.send_messsage(channel, "{} has leveled up to {}".format(user.mention, lvl_end))
+		users[user.id]['level'] = lvl_end
+		await client.process_commands(message)
+	
+		
 @client.event
 async def on_message_delete(message):
     author = message.author
@@ -59,7 +102,7 @@ async def on_message_delete(message):
 @client.event
 async def on_reaction_add(reaction, user):
     channel = reaction.message.channel
-    await client.send_message(channel, '{} has added {} to the message: {}'.format(user.name, reaction.emoji, reaction.message.content))
+    await client.send_message(channel, '{} has added {} to the message: {}'.format(user.name, reaction.emoji ,reaction.message.content))
     await client.process_commands(message)
 
 
